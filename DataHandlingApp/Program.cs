e45d61242ecdfc4e883f1065031f44ceb6836f0a
Laurits Bentsen <laurits2@hotmail.com>
2024-01-23 12:25:34 +0100

    Initialisation of repo for data management and handling application

---
+++ DataHandlingApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace SQLite_project
+{ 
+    class Program
+    {
+        static void Main()
+        { 
+            using (var connection = new SqliteConnection("Data Source=/Users/Cecilie/Projects/WineAppDataFile.sqlite"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    SELECT city
+                    FROM soil_data
+                ";
+
+
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var city = reader.FieldCount;
+
+                        Console.WriteLine($"{city}");
+                    }
+                }
+            }
+        }
+    }
+}
